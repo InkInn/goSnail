6e61792eba1cd357135e1392ef0d259109a415a7
InkInn <lrc19931109@outlook.com>
2020-05-15 13:49:45 +0800

    update

---
+++ hello/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/InkInn/goSnail/stirngutil"
+)
+
+func main() {
+	fmt.Printf(stirngutil.Reverse("Hello, world.\n"))
+}
---
+++ stirngutil/reverse_test.go
@@ -0,0 +1,24 @@
+package stirngutil
+
+import "testing"
+
+func TestReverse(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"1",args{"12345"},"54321"},
+		{"2",args{"67890"},"09876"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Reverse(tt.args.s); got != tt.want {
+				t.Errorf("Reverse() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
